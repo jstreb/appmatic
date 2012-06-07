dd7d97d6bc87e4e5412256a587de7929718dcc13
Jesse Streb <jesse.streb@gmail.com>
2012-06-07 18:54:36 -0400

    have caching working

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,22 @@ var AUTH_TOKEN = "14f9ad049e4b036b56047a529";
 var ACCOUNT_ID = "665003303001";
 var ACCOUNT_LIMIT_IN_GB = 40;
 var MEDIA_API_TOKEN = "7kITGpNoLcepAAN-8u7b--bguf8F_S5pxbVFh7l8ihCoiMkqltT0JA..";
+var TIMES = [720, 1440, 10080];
 
+var cached = { 
+  "720cache": 0
+, "1440cache": 0
+, "10080cache": 0
+};
 
+//Keep refreshing this data
+setInterval( makeMetricCalls, 180000);
+
+function makeMetricCalls() {
+  for( var i=0; i<TIMES.length; i++) {
+     fetchMetrics( TIMES[i]);
+   }
+}
 
 var makeAPICall = function(path, callback){
   console.log( path );
@@ -101,13 +115,11 @@ function formatData( vc, ac, al, to, from ) {
  return ret;
 }
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
-};
-
-exports.metrics = function(req, res){
+function fetchMetrics( from ) {
+  console.log( "calling fetch metrics for: " + from ); 
+  var cacheKey = from + "cache";
   var to = new Date().getTime();
-  var from = to - (req.params.from * 1000 * 60);
+  var from = to - (from * 1000 * 60);
   var videoMetrics;
   var metaData;
   var accountMetrics;
@@ -115,7 +127,7 @@ exports.metrics = function(req, res){
   makeAPICall( '/analytics-api/data/videocloud/account/' + ACCOUNT_ID + "?from=" + from + "&to=" + to, function( data ) {
     accountMetrics = JSON.parse( data );
     if( metaData ) {
-      res.send( formatData( metaData, videoMetrics, accountMetrics, to, from ) );
+      cached[cacheKey] = formatData( metaData, videoMetrics, accountMetrics, to, from );
     }
   });
   
@@ -128,12 +140,27 @@ exports.metrics = function(req, res){
     makeMediaAPICall( "/services/library?command=find_videos_by_ids&token=" + MEDIA_API_TOKEN + "&video_ids=" + videoIDs + "&video_fields=id%2Cname%2CvideoStillURL%2CshortDescription&get_item_count=true", function(data) {
       metaData = JSON.parse( data ).items;
       if( accountMetrics ) {
-        res.send( formatData( metaData, videoMetrics, accountMetrics, to, from ) );
+        if( metaData ) {
+          cached[cacheKey] = formatData( metaData, videoMetrics, accountMetrics, to, from );
+        }
       }
     });
   });
   
+}
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' })
 };
+
+exports.metrics = function(req, res){
+  //to - (req.params.from * 1000 * 60);
+  var from = req.params.from;
+  var cacheKey = from + "cache";
+  res.send( cached[cacheKey] );
+};
+
+makeMetricCalls();
 
 exports.usage = function(req, res){
   var to = new Date().getTime();
