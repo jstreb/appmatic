70f8b327d2e59b508d3dd3745f61893ac370613a
Jesse Streb <jstreb@brightcove.com>
2012-06-08 08:02:39 -0400

    making the address configurable by heroku

--- server/app.js
+++ server/app.js
@@ -8,6 +8,8 @@ var express = require('express')
 
 var app = module.exports = express.createServer();
 
+var port = process.env.PORT || 3000;
+
 // Configuration
 
 app.configure(function(){
@@ -34,6 +36,6 @@ app.get('/metrics/:from', routes.metrics);
 app.get('/usage', routes.usage);
 app.post('/settings', routes.settings );
 
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
